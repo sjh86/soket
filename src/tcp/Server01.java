5e174096e046c10b02720bc0a08e6aedd3a2e77d
unknown <107wkd@naver.com>
2021-09-10 15:10:13 +0900

    soket

---
+++ src/tcp/Server01.java
@@ -0,0 +1,16 @@
+package tcp;
+
+import java.net.ServerSocket;
+import java.io.IOException;
+
+public class Server01 {
+	public static void main (String[] args) throws Exception{
+		ServerSocket server = new ServerSocket(12345);
+		
+		
+		
+		
+		
+		
+	}
+}
